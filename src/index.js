4f44d60e7a4d8d57e72fbfd86af118fb3f84e9e6
Kellen Busby <kellenbusby@gmail.com>
2021-10-01 18:37:37 -0600

    100% heights with position fixed

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-const setViewHeight = () => {
-  console.log('setting vh')
-  const vh = window.innerHeight * 0.01
-  document.documentElement.style.setProperty('--vh', `${vh}px`)
-}
+// const setViewHeight = () => {
+//   console.log('setting vh')
+//   const vh = window.innerHeight * 0.01
+//   document.documentElement.style.setProperty('--vh', `${vh}px`)
+// }
 
-setViewHeight()
+// setViewHeight()
 
-window.addEventListener('resize', setViewHeight)
-window.addEventListener('touchend', setViewHeight)
+// window.addEventListener('resize', setViewHeight)
+// window.addEventListener('touchstart', () => {
+//   setViewHeight()
+// })
 
 ReactDOM.render(
   <React.StrictMode>
