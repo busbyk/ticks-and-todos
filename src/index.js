39d7fa946c54108ac3b3a89a0af85bc2cbe344b6
Kellen Busby <kellenbusby@gmail.com>
2021-10-01 18:02:48 -0600

    fixing event listener being attached to document not window

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.css'
 import App from './App'
 
 const setViewHeight = () => {
+  console.log('setting vh')
   const vh = window.innerHeight * 0.01
   document.documentElement.style.setProperty('--vh', `${vh}px`)
 }
 
 setViewHeight()
 
-document.addEventListener('resize', () => {
+window.addEventListener('resize', () => {
   setViewHeight()
 })
 
