ae2a0136cfc0bd321069fcc6c056424d0c89af05
Kellen Busby <kellenbusby@gmail.com>
2021-10-03 11:15:19 -0600

    100vh on all works best, adding a margin at the bottom so no content is hidden on first scroll

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-const setViewHeight = () => {
-  const vh = window.innerHeight * 0.01
-  document.documentElement.style.setProperty('--vh', `${vh}px`)
-}
-
-window.addEventListener('resize', setViewHeight)
-window.addEventListener('touchend', setViewHeight)
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
