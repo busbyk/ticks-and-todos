c1c83923708a8f57d59f1b8d3061f531fcd0b169
Kellen Busby <kellenbusby@gmail.com>
2021-10-03 11:09:22 -0600

    full vh on html, body, and #root and resize it using innerHeight

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+const setViewHeight = () => {
+  const vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+}
+
+setViewHeight()
+
+window.addEventListener('resize', setViewHeight)
+window.addEventListener('touchend', setViewHeight)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
